099ee6406adbd8d7fb92e7627fc934e01917c130
Andres Suarez <realbarcelona339@gmail.com>
2026-07-28 16:35:16 -0500

    Hemos hecho un comentario mas formal

--- holaPrimerArchivo.java
+++ holaPrimerArchivo.java
@@ -1,5 +1,5 @@
 public class holaPrimerArchivo {
     public static void main(String[] args) {
-        System.out.println("ya vamos avanzando");
+        System.out.println("Hemos mejorado mucho, vamos por mas");
     }
 }
